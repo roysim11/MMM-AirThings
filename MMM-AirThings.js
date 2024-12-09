ba2b0e4ba14d24a40fc332e142a3f1732a69810a
roysim11 <ddicostanzi86@gmail.com>
2024-12-09 11:08:14 -0500

    Create MMM-AirThings.js

---
+++ MMM-AirThings.js
@@ -0,0 +1,53 @@
+Module.register("MMM-AirThings", {
+    defaults: {
+        updateInterval: 10 * 60 * 1000, // Update every 10 minutes
+        airThingsToken: "YOUR_API_TOKEN",
+        sensorId: null // Replace with your specific AirThings sensor ID
+    },
+
+    start: function () {
+        this.loaded = false;
+        this.sensorData = null;
+        this.getData();
+        setInterval(() => this.getData(), this.config.updateInterval);
+    },
+
+    getData: function () {
+        this.sendSocketNotification("FETCH_AIRTHINGS_DATA", {
+            token: this.config.airThingsToken,
+            sensorId: this.config.sensorId
+        });
+    },
+
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "AIRTHINGS_DATA_RECEIVED") {
+            this.sensorData = payload;
+            this.loaded = true;
+            this.updateDom();
+        }
+    },
+
+    getDom: function () {
+        const wrapper = document.createElement("div");
+
+        if (!this.loaded) {
+            wrapper.innerHTML = "Loading AirThings data...";
+            return wrapper;
+        }
+
+        if (!this.sensorData) {
+            wrapper.innerHTML = "No data available.";
+            return wrapper;
+        }
+
+        wrapper.innerHTML = `
+            <div>
+                <h2>AirThings Data</h2>
+                <p>Temperature: ${this.sensorData.temperature} °C</p>
+                <p>Humidity: ${this.sensorData.humidity} %</p>
+                <p>CO2: ${this.sensorData.co2} ppm</p>
+            </div>
+        `;
+        return wrapper;
+    }
+});
